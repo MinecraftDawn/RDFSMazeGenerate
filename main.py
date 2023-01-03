a04dafd30df2ccc24dd584140b44e9cd0026c7e4
MinecraftDawn <eric4902077@gmail.com>
2023-01-04 00:40:25 +0800

    aStar

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import random
 from collections import deque
-ROW = 40
-COL = 40
+import heapq
+
+ROW = 10
+COL = 10
 MAZE = [[[1, 0] for _ in range(COL)] for _ in range(ROW)]
 
 INIT_X, INIT_Y = 0, 0
@@ -81,18 +83,12 @@ def bfs(maze):
         visted[r][c] = True
         #if r == len(maze)-2 and c == len(maze[0]) -2: break
         for DIR in DIRECTS:
-            R,C = DIR
+            R, C = DIR
             nr, nc = r+R, c+C
             if 0 < nr < len(maze) and 0 < nc < len(maze[0]) and maze[nr][nc] == 0 and not visted[nr][nc]:
                 queue.append((nr, nc))
-                if DIR == [0, 1]:
-                    tmp[nr][nc] = [0, 1]
-                elif DIR == [1, 0]:
-                    tmp[nr][nc] = [1, 0]
-                elif DIR == [-1, 0]:
-                    tmp[nr][nc] = [-1, 0]
-                elif DIR == [0, -1]:
-                    tmp[nr][nc] = [0, -1]
+                tmp[nr][nc] = DIR.copy()
+
 
     # for r in range(len(tmp)):
     #     for c in range(len(tmp[0])):
@@ -105,11 +101,111 @@ def bfs(maze):
     r, c = len(tmp)-2, len(tmp[0])-2
     while tmp[r][c] != [0, 0]:
         maze[r][c] = 2
-        dr,dc = tmp[r][c]
-        r,c = r-dr,c-dc
+        dr, dc = tmp[r][c]
+        r, c = r-dr, c-dc
     maze[1][1] = 2
 
     return maze
+
+
+def evalH(cur, end):
+    return abs(cur[0] - end[0]) + abs(cur[1] - end[1])
+
+def evalF(g, h, a=1, b=1):
+    return a*g + b*h
+
+
+class Node():
+    def __init__(self, parent=None, pos=None):
+        self.parent = parent
+        self.pos = pos
+
+        self._g = 0
+        self._h = 0
+
+    @property
+    def g(self):
+        return self._g
+
+    @g.setter
+    def g(self, g):
+        self._g = g
+
+    @property
+    def h(self):
+        return self._h
+
+    @h.setter
+    def h(self, h):
+        self._h = h
+
+    @property
+    def f(self):
+        return self.g + self.h
+
+    def evalH(self, end):
+        return abs(self.pos[0] - end[0]) + abs(self.pos[1] - end[1])
+
+    def __eq__(self, other):
+        return self.pos == other.pos
+
+def findMinNode(nodes):
+    findNode = nodes[0]
+    findIndex = 0
+    for index, node in enumerate(nodes):
+        if node.f < findNode.f:
+            findNode = node
+            findIndex = index
+
+    return findNode, findIndex
+
+def aStar(maze):
+    endR, endC = len(maze)-2, len(maze[0])-2
+
+    startNode = Node(None, (1, 1))
+    #start.h = start.evalH((endR,endC))
+
+    endNode = Node(None, (endR, endC))
+
+    open = [startNode]
+    close = []
+
+    while open:
+        curNode, curIndex = findMinNode(open)
+
+        open.pop(curIndex)
+        close.append(curNode)
+
+        if curNode == endNode:
+            path = []
+            tmp = curNode
+            while tmp:
+                path.append(tmp.pos)
+                tmp = tmp.parent
+            return path
+
+        childs = []
+        for DIR in DIRECTS:
+            R, C = DIR
+            nr, nc = curNode.pos[0] + R, curNode.pos[1] + C
+            if 0 < nr < len(maze) and 0 < nc < len(maze[0]) and maze[nr][nc] == 0:
+
+                nextNode = Node(curNode, (nr, nc))
+                childs.append(nextNode)
+
+        for child in childs:
+
+            if child in close:
+                continue
+
+            child.g = curNode.g + 1
+            child.h = child.evalH((endR,endC))
+
+            for openNode in open:
+                if child == openNode and child.g > openNode.g:
+                    continue
+
+            open.append(child)
 
 
 def printMaze(maze):
@@ -127,5 +223,9 @@ rdfs(0, 0)
 
 maze = generateMaze()
 
-maze = bfs(maze)
+# maze = bfs(maze)
+path = aStar(maze)
+for pos in path:
+    r,c = pos
+    maze[r][c] = 2
 printMaze(maze)
